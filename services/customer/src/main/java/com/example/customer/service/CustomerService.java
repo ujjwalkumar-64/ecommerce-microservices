5811b7ae8a94720e9bf08ff5581f1eeb46c0afa8
ujjwalkumar-64 <ujjwalkumar6281@gmail.com>
2025-09-28 00:03:48 +0530

    customer service

---
+++ services/customer/src/main/java/com/example/customer/service/CustomerService.java
@@ -0,0 +1,8 @@
+package com.example.customer.service;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public interface CustomerService  {
+
+}
---
+++ services/customer/src/main/java/com/example/customer/service/impl/CustomerServiceImpl.java
@@ -0,0 +1,8 @@
+package com.example.customer.service.impl;
+
+import com.example.customer.service.CustomerService;
+import org.springframework.stereotype.Service;
+
+@Service
+public class CustomerServiceImpl implements CustomerService {
+}

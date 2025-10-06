5969c305580157940ae8e11901751b23aa8133a1
ujjwalkumar-64 <ujjwalkumar6281@gmail.com>
2025-10-07 01:51:25 +0530

    keycloak setup and configured on gateway service

---
+++ services/gateway/src/main/java/com/microservice/gateway/security/SecurityConfig.java
@@ -0,0 +1,30 @@
+package com.microservice.gateway.security;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.reactive.EnableWebFluxSecurity;
+import org.springframework.security.config.web.server.ServerHttpSecurity;
+import org.springframework.security.web.server.SecurityWebFilterChain;
+
+@EnableWebFluxSecurity
+@Configuration
+public class SecurityConfig {
+
+    @Bean
+    public SecurityWebFilterChain securityWebFilterChain(ServerHttpSecurity serverHttpSecurity){
+        serverHttpSecurity
+                .csrf(ServerHttpSecurity.CsrfSpec :: disable)
+                .authorizeExchange(exchange -> {
+                    exchange.pathMatchers("/eureka/**")
+                            .permitAll()
+                            .anyExchange()
+                            .authenticated();
+                })
+                .oauth2ResourceServer(oauth2ResourceServer -> {
+                    oauth2ResourceServer.jwt(Customizer.withDefaults());
+                });
+
+        return serverHttpSecurity.build();
+    }
+}
